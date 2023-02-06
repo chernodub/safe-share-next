217fee7470e8aeb814509a6ed3d5c0accf62168f
Viktor Chernodub <chernodubv@gmail.com>
2023-02-06 10:38:59 +0300

    feat: add system-based theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { type AppType } from 'next/app';
@@ -6,13 +6,15 @@ import { type AppType } from 'next/app';
 import { api } from '../utils/api';
 
 import '../styles/globals.css';
+import { theme } from '../utils/theme';
 
 const MyApp: AppType<{ session: Session | null; }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => (
   <SessionProvider session={session}>
-    <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   </SessionProvider>
---
+++ src/utils/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'system',
+  useSystemColorMode: true,
+};
+
+export const theme = extendTheme({ config }) as {
+  config: typeof config;
+};
