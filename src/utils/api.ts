c9ee96949af4533a45acb363f2572c6e81c96397
Viktor Chernodub <chernodubv@gmail.com>
2023-02-08 12:15:21 +0300

    chore: adjust formatting

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,11 +14,11 @@ import { type AppRouter } from '../server/api/root';
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') {
-return '';
-} // browser should use relative url
+    return '';
+  } // browser should use relative url
   if (process.env.VERCEL_URL) {
-return `https://${process.env.VERCEL_URL}`;
-} // SSR should use vercel url
+    return `https://${process.env.VERCEL_URL}`;
+  } // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 };
 
@@ -40,7 +40,8 @@ export const api = createTRPCNext<AppRouter>({
        */
       links: [
         loggerLink({
-          enabled: opts => process.env.NODE_ENV === 'development' ||
+          enabled: opts =>
+            process.env.NODE_ENV === 'development' ||
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
         httpBatchLink({
